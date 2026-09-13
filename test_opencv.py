3e0f88f05ddaccd600defb268c08eef55c36ce73
Abraham Lopez <abethebest111@gmail.com>
2026-09-12 23:51:24 -0700

    test video opening success

--- test_opencv.py
+++ test_opencv.py
@@ -1,3 +1,17 @@
 import cv2
 
-print("OpenCV version:", cv2.__version__)
+video = cv2.VideoCapture("videos/abrahamtrainingtest.mov")
+
+while True:
+    success, frame = video.read()
+
+    if not success:
+        break
+
+    cv2.imshow("Test Video", frame)
+
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+        break
+
+video.release()
+cv2.destroyAllWindows()
