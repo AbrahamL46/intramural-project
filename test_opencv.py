c925610e89dc72775668a7de8b3398a28af72de5
Abraham Lopez <abethebest111@gmail.com>
2026-09-13 00:44:21 -0700

    ignore local videos and project files

--- test_opencv.py
+++ test_opencv.py
@@ -2,11 +2,26 @@ import cv2
 
 video = cv2.VideoCapture("videos/abrahamtrainingtest.mov")
 
+#video information
+fps = video.get(cv2.CAP_PROP_FPS)
+width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print("FPS:", fps)
+print("Width:", width)
+print("Height:", height)
+
 while True:
     success, frame = video.read()
 
     if not success:
         break
+
+    #draw rectangle
+    cv2.rectangle(frame, (100, 100), (400, 300), (0, 255, 0), 2)
+
+    #draw circle
+    cv2.circle(frame, (250, 200), 10, (0, 0, 255), -1)
 
     cv2.imshow("Test Video", frame)
 
