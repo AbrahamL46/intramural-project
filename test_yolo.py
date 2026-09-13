0ff8e1001be95e26854da71d56e9733d8a220c1c
Abraham Lopez <abethebest111@gmail.com>
2026-09-13 16:39:26 -0700

    testing information gotten from picture

--- test_yolo.py
+++ test_yolo.py
@@ -6,6 +6,16 @@ model = YOLO("yolo11n.pt")
 #run detection on an image
 results = model("videos/test_image.jpg")
 
-#show image with YOLO's boxes drawn on it
-results[0].show()
+result = results[0]
 
+for box in result.boxes:
+    class_id = int(box.cls[0])
+    confidence = float(box.conf[0])
+
+    name = result.names[class_id]
+
+    print(name, confidence)
+
+    x1, y1, x2, y2 = box.xyxy[0]
+
+    print(x1, y1, x2, y2)
